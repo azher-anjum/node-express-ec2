613ac0790dfe075ada73bb9584c142c81eaaece6
azher-anjum'git config --global user.email azher.anjum@gmail.com <azher.anjum@gmail.com>
2024-07-31 16:03:59 +0530

    my first ec2 commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(5001, () => console.log("api running on port 5001 HELLO WORLD"))
+
+app.get('/', (req,res)=> res.json('my api running so hello world'))
