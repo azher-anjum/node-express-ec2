fc6cd629bed6917bf3b39ce2d64b81870dc3549d
azher-anjum'git config --global user.email azher.anjum@gmail.com <azher.anjum@gmail.com>
2024-08-02 14:16:12 +0530

    new file

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const app = express()
 
 app.listen(5001, () => console.log("api running on port 5001 HELLO WORLD"))
 
-app.get('/', (req,res)=> res.json("4th time api hit "))
-console.log("hello world")
+app.get('/', (req,res)=> res.json("hello world "))
